f436bdcb65d79b4b4c53b6fa9ca182925e0188a9
trekk-8472 <trekker.8472.rp@gmail.com>
2025-10-11 19:33:40 -0400

    added node stuff from pdf

--- Sequence.h
+++ Sequence.h
@@ -73,3 +73,19 @@ public:
     friend ostream& operator<<(ostream& os, const Sequence& s);
 };
 
+class SequenceNode {
+public: // to make it easier, we can make the data members public so we don't need
+    // getters and setters
+    SequenceNode* next; // pointer to next Node. If node is the tail, next is
+    // nullptr
+    SequenceNode* prev; // pointer to previous Node. If node is the head, prev is
+    // nullptr
+    std::string item; // the element being stored in the node
+    //default constructor, ensure next and prev are nullptr
+    SequenceNode() : next(nullptr), prev(nullptr)
+    {}
+    /// parameterized constructor, next and prev are set to nullptr and the
+    /// node's element is set to the given value
+    SequenceNode(std::string item) : next(nullptr), prev(nullptr), item(item)
+    {}
+};
