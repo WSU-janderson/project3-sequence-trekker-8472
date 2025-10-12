374147d2b05d9d2c68fb0a7d014adc2b56cb6603
trekk-8472 <trekker.8472.rp@gmail.com>
2025-10-12 12:52:35 -0400

    forgot the change to the push notes... fixed somme logic and allocation errors. and added semipointless comment

--- Sequence.cpp
+++ Sequence.cpp
@@ -95,7 +95,7 @@ void Sequence::insert(size_t position, std::string item) {
 
     if ( position >= 0 && position <= size() ) {//for some reason instructions have last_item instead of size(below)
 
-        SequenceNode* current = head;
+        SequenceNode* current = head; //start sequential run if necessary from head
         SequenceNode* newNode = new SequenceNode(item);//create the item
 
         if ( position == size() ) { //avoid seccond cALL for newNode by copying logic from within pushbak
