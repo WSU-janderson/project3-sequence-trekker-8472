d42f836e806c0f253d50c164e9e49e0185c144ff
trekker-8472 <92266613+trekker-8472@users.noreply.github.com>
2025-10-15 15:34:46 -0400

    Update SequenceDebug.cpp
    added in name to opening notes

--- SequenceDebug.cpp
+++ SequenceDebug.cpp
@@ -2,7 +2,7 @@
  * SequenceDebug.cpp
  * Project 3
  * CS 3100
- *
+ * Robert Pohl
  * In this file, you will write your tests as you implement Sequence. If you are using CLion, you need to select
  * SequenceDebug from the drop-down menu next to the Build (hammer icon) if it is on SequenceTestHarness
  */
